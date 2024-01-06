b3986e5c8667f4c7e3ee3300b61ebe46bfdfefe8
Tizekod <noon.tizekod@gmail.com>
2024-01-05 21:26:26 -0500

    Adds MakeBricks problem to the project

---
+++ src/PrepApp/codingbat/MakeBricks/MakeBricks.java
@@ -0,0 +1,7 @@
+package PrepApp.codingbat.MakeBricks;
+
+public class MakeBricks {
+    public boolean makeBricks(int small, int big, int goal){
+        return false;
+    }
+}
--- src/Tests/DSATests.java
+++ src/Tests/DSATests.java
@@ -1,6 +1,7 @@
 package Tests;
 import static org.junit.jupiter.api.Assertions.*;
 
+import PrepApp.codingbat.MakeBricks.MakeBricks;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -9,11 +10,13 @@ import PrepApp.codingbat.EvenlySpaced.EvenlySpaced;
 class DSATests {
 	
 	private EvenlySpaced evenly;
+	private MakeBricks brickMaker;
 	
 
 	@BeforeEach
 	void setUp() throws Exception {
 		evenly = new EvenlySpaced();
+		brickMaker = new MakeBricks();
 	}
 
 	@Test
@@ -21,6 +24,13 @@ class DSATests {
 		assertTrue(evenly.evenlySpaced(4, 6, 3), "Evenly Spaced if returns true");
 		assertTrue(evenly.evenlySpaced(2, 4, 6), "Evenly Spaced if returns true");
 		assertTrue(evenly.evenlySpaced(4, 6, 2), "Evenly Spaced if returns true");
+	}
+
+	@Test
+	void testBrickMaking() {
+		assertTrue(brickMaker.makeBricks(3, 1, 8), "Returns true if it possible to make the goal by choosing from the given Bricks");
+		assertTrue(brickMaker.makeBricks(3, 1, 9), "Returns true if it possible to make the goal by choosing from the given Bricks");
+		assertTrue(brickMaker.makeBricks(3, 2, 10), "Returns true if it possible to make the goal by choosing from the given Bricks");
 	}
 	
 
