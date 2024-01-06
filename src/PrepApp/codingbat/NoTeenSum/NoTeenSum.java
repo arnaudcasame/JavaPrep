d0c060f0e501906457d56a46164c4dbfe3fe36ca
Tizekod <noon.tizekod@gmail.com>
2024-01-06 01:15:37 -0500

    Adds the CodingBat problem NoTeenSum to the project

---
+++ src/PrepApp/codingbat/NoTeenSum/NoTeenSum.java
@@ -0,0 +1,8 @@
+package PrepApp.codingbat.NoTeenSum;
+
+public class NoTeenSum {
+
+    public int calculateSum(int a, int b, int c){
+        return 0;
+    }
+}
--- src/Tests/DSATests.java
+++ src/Tests/DSATests.java
@@ -1,6 +1,7 @@
 package Tests;
 import static org.junit.jupiter.api.Assertions.*;
 
+import PrepApp.codingbat.NoTeenSum.NoTeenSum;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -13,6 +14,7 @@ class DSATests {
 	private EvenlySpaced evenly;
 	private MakeBricks brickMaker;
 	private MaximumProfit profitCalculator;
+	private NoTeenSum sumCalculator;
 	
 
 	@BeforeEach
@@ -20,6 +22,7 @@ class DSATests {
 		evenly = new EvenlySpaced();
 		brickMaker = new MakeBricks();
 		profitCalculator = new MaximumProfit();
+		sumCalculator = new NoTeenSum();
 	}
 
 	@Test
@@ -41,6 +44,12 @@ class DSATests {
 		assertEquals(120, profitCalculator.calculateProfit(new int[]{1,2,3,3}, new int[]{3,4,5,6}, new int[]{50,10,40,70}), "Maximum Profit with no two jobs overlapping time range");
 		assertEquals(150, profitCalculator.calculateProfit(new int[]{1,2,3,4,6}, new int[]{3,5,10,6,9}, new int[]{20,20,100,70,60}), "Maximum Profit with no two jobs overlapping time range");
 		assertEquals(6, profitCalculator.calculateProfit(new int[]{1,1,1}, new int[]{2,3,4}, new int[]{5,6,4}), "Maximum Profit with no two jobs overlapping time range");
+	}
+	@Test
+	void testNoTeenSum() {
+		assertEquals(6, sumCalculator.calculateSum(1, 2, 3));
+		assertEquals(3, sumCalculator.calculateSum(2, 13, 1));
+		assertEquals(3, sumCalculator.calculateSum(2, 1, 14));
 	}
 	
 
