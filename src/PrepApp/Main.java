2c9669e7bd42d5189f7262a1e0a455a5de163c36
Tizekod <noon.tizekod@gmail.com>
2024-01-06 17:41:20 -0500

    Implements CodingBat doubling solution

--- src/PrepApp/Main.java
+++ src/PrepApp/Main.java
@@ -1,7 +1,13 @@
 package PrepApp;
 
+import PrepApp.codingbat.FunctionalMapping.FunctionalMapping;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        FunctionalMapping func = new FunctionalMapping();
+        func.doubling(Arrays.asList(new Integer[]{1, 2, 3}));
     }
 }
--- src/PrepApp/codingbat/FunctionalMapping/FunctionalMapping.java
+++ src/PrepApp/codingbat/FunctionalMapping/FunctionalMapping.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 public class FunctionalMapping {
     public List<Integer> doubling(List<Integer> nums){
-        return new ArrayList<Integer>();
+        nums.replaceAll(n -> n*2);
+        System.out.println(nums);
+        return nums;
     }
 }
