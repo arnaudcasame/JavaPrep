1d8e4f3b00806fc7916a408091f09eeb1ac253d0
Tizekod <noon.tizekod@gmail.com>
2024-01-06 18:07:21 -0500

    Fixes issues in CodingBat doubling solution

--- src/PrepApp/Main.java
+++ src/PrepApp/Main.java
@@ -3,11 +3,10 @@ package PrepApp;
 import PrepApp.codingbat.FunctionalMapping.FunctionalMapping;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        FunctionalMapping func = new FunctionalMapping();
-        func.doubling(Arrays.asList(new Integer[]{1, 2, 3}));
     }
 }
--- src/PrepApp/codingbat/FunctionalMapping/FunctionalMapping.java
+++ src/PrepApp/codingbat/FunctionalMapping/FunctionalMapping.java
@@ -5,8 +5,10 @@ import java.util.List;
 
 public class FunctionalMapping {
     public List<Integer> doubling(List<Integer> nums){
+        if(nums.isEmpty()){
+            return List.of();
+        }
         nums.replaceAll(n -> n*2);
-        System.out.println(nums);
         return nums;
     }
 }
