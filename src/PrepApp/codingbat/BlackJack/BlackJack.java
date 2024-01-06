e6c379c430c20a01c900deed10d6f2869fb0f795
Tizekod <noon.tizekod@gmail.com>
2024-01-06 02:22:16 -0500

    Solves the Coding Bat BlackJack problem

--- src/PrepApp/codingbat/BlackJack/BlackJack.java
+++ src/PrepApp/codingbat/BlackJack/BlackJack.java
@@ -3,6 +3,18 @@ package PrepApp.codingbat.BlackJack;
 public class BlackJack {
 
     public int calculateNearest(int a, int b){
-        return 0;
+        if(a > 21 && b > 21){
+            return 0;
+        }
+        if(a > 21 && b < 21){
+            return b;
+        }
+        if(a < 21 && b > 21){
+            return a;
+        }
+        if(21 - a > 21 - b){
+            return b;
+        }
+        return a;
     }
 }
