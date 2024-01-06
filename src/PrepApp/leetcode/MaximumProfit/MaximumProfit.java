eb9166c849d2f5a9b1d14172813cb1849e9f24b7
Tizekod <noon.tizekod@gmail.com>
2024-01-05 23:50:37 -0500

    Adds the leetcode problem 1235 Maximum Profit to the project

---
+++ src/PrepApp/leetcode/MaximumProfit/MaximumProfit.java
@@ -0,0 +1,8 @@
+package PrepApp.leetcode.MaximumProfit;
+
+public class MaximumProfit {
+
+    public int calculateProfit(int[] startTime, int[] endTime, int[] profit){
+        return 0;
+    }
+}
--- src/Tests/DSATests.java
+++ src/Tests/DSATests.java
@@ -1,22 +1,25 @@
 package Tests;
 import static org.junit.jupiter.api.Assertions.*;
 
-import PrepApp.codingbat.MakeBricks.MakeBricks;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import PrepApp.codingbat.MakeBricks.MakeBricks;
 import PrepApp.codingbat.EvenlySpaced.EvenlySpaced;
+import PrepApp.leetcode.MaximumProfit.MaximumProfit;
 
 class DSATests {
 	
 	private EvenlySpaced evenly;
 	private MakeBricks brickMaker;
+	private MaximumProfit profitCalculator;
 	
 
 	@BeforeEach
 	void setUp() throws Exception {
 		evenly = new EvenlySpaced();
 		brickMaker = new MakeBricks();
+		profitCalculator = new MaximumProfit();
 	}
 
 	@Test
@@ -31,6 +34,13 @@ class DSATests {
 		assertTrue(brickMaker.makeBricks(3, 1, 8), "Returns true if it possible to make the goal by choosing from the given Bricks");
 		assertTrue(brickMaker.makeBricks(3, 1, 9), "Returns true if it possible to make the goal by choosing from the given Bricks");
 		assertTrue(brickMaker.makeBricks(3, 2, 10), "Returns true if it possible to make the goal by choosing from the given Bricks");
+	}
+
+	@Test
+	void testProfitCalculator() {
+		assertEquals(120, profitCalculator.calculateProfit(new int[]{1,2,3,3}, new int[]{3,4,5,6}, new int[]{50,10,40,70}), "Maximum Profit with no two jobs overlapping time range");
+		assertEquals(150, profitCalculator.calculateProfit(new int[]{1,2,3,4,6}, new int[]{3,5,10,6,9}, new int[]{20,20,100,70,60}), "Maximum Profit with no two jobs overlapping time range");
+		assertEquals(6, profitCalculator.calculateProfit(new int[]{1,1,1}, new int[]{2,3,4}, new int[]{5,6,4}), "Maximum Profit with no two jobs overlapping time range");
 	}
 	
 
