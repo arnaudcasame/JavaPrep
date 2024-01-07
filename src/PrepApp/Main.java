c59c0e60f36aa3ac5a0786818a005dc44400a798
Tizekod <noon.tizekod@gmail.com>
2024-01-06 22:34:29 -0500

    Removes unnecessary codes

--- src/PrepApp/Main.java
+++ src/PrepApp/Main.java
@@ -1,10 +1,5 @@
 package PrepApp;
 
-import PrepApp.codingbat.FunctionalMapping.FunctionalMapping;
-
-import java.util.Arrays;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
--- src/PrepApp/codingbat/FunctionalMapping/FunctionalMapping.java
+++ src/PrepApp/codingbat/FunctionalMapping/FunctionalMapping.java
@@ -1,6 +1,5 @@
 package PrepApp.codingbat.FunctionalMapping;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class FunctionalMapping {
