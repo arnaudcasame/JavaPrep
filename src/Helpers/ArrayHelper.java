af25e6408992f9b145b558835e986d993aafa5db
Tizekod <noon.tizekod@gmail.com>
2024-03-02 00:34:59 -0500

    Implements merge sort algorithm

--- src/Helpers/ArrayHelper.java
+++ src/Helpers/ArrayHelper.java
@@ -1,6 +1,10 @@
 package Helpers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ArrayHelper<T> {
 
@@ -22,5 +26,80 @@ public class ArrayHelper<T> {
             res[j] = inner;
         }
         return res;
+    }
+
+    public int[] mergeSortArray(int[] nums){
+
+        if(nums.length == 1){
+            return nums;
+        }
+
+        int[] left = this.splitArray(nums)[0];
+        int[] right = this.splitArray(nums)[1];
+
+        mergeSortArray(left);
+        mergeSortArray(right);
+
+//        System.out.println(Arrays.toString(nums) + " Left: " + Arrays.toString(left) + " Right: " + Arrays.toString(right));
+
+        return merge(left, right);
+    }
+
+    private int[][] splitArray(int[] nums){
+        int mid = (int) nums.length/2;
+        int[] left = new int[mid];
+        int[] right = (nums.length%2)==0 ? new int[mid] : new int[mid+1];
+
+        for (int i = 0; i < mid; i++) {
+            left[i] = nums[i];
+        }
+
+        for (int i = mid; i < nums.length; i++) {
+            right[i-mid] = nums[i];
+        }
+
+        int[][] result = new int[2][mid];
+
+        result[0] = left;
+        result[1] = right;
+
+        return result;
+    }
+
+    private int[] merge(int[] left, int[] right){
+        List<Integer> tmp = new ArrayList<>();
+
+        int i=0;
+        int j=0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] < right[j]){
+                tmp.add(left[i]);
+                i++;
+            }else{
+                tmp.add(right[j]);
+                j++;
+            }
+        }
+
+        while (j < right.length) {
+            tmp.add(right[j]);
+            j++;
+        }
+
+        while (i < left.length) {
+            tmp.add(left[i]);
+            i++;
+        }
+
+        Collections.sort(tmp);
+
+        System.out.println(tmp);
+        int[] result = new int[tmp.size()];
+        for (int k = 0; k < tmp.size(); k++) {
+            result[k] = tmp.get(k);
+        }
+
+        return  result;
     }
 }
