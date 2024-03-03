f94bff585cc1ccdc4a315e5d5ebee96c3ea92fdd
Tizekod <noon.tizekod@gmail.com>
2024-03-03 16:29:38 -0500

    Implements utility functions for ListNode

--- src/DSA/ListNode.java
+++ src/DSA/ListNode.java
@@ -1,5 +1,8 @@
 package DSA;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListNode<T> {
     private final T val;
     private ListNode<T> next;
@@ -18,5 +21,33 @@ public class ListNode<T> {
 
     public void setNext(ListNode<T> next) {
         this.next = next;
+    }
+
+    public static ListNode<Integer> fromArray(int[] arr){
+        ListNode<Integer> head = null;
+        ListNode<Integer> current = null;
+        for(int item: arr){
+            if(head == null){
+                head = new ListNode<Integer>(item, null);
+                current = head;
+            }else {
+                current.next = new ListNode(item, null);
+                current = current.next;
+            }
+        }
+        return head;
+    }
+
+    public static int[] toArray(ListNode<Integer> head) {
+        List<Integer> list = new ArrayList<>();
+        while (head != null) {
+            list.add(head.getVal());
+            head = head.next;
+        }
+        int[] result = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+        return result;
     }
 }
